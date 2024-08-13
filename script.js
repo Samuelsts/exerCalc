30c12492f2818eb8e1509b410510a46d54998c5a
Samuel Santos <71991685+Samuelsts@users.noreply.github.com>
2024-08-12 22:21:52 -0300

    foi definido logica e operações

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const tclRes=document.querySelector('.res');
 const display =document.querySelector('.display');
 const tOn =document.querySelector('#ton');
 const tLimpar =document.querySelector('#tlimpar');
+const tigual =document.querySelector('#tigual');
+
+
+let sinal=false
+decimal=false
 
 console.log(tclNum)
 console.log(tclOp)
@@ -11,15 +16,50 @@ console.log(tclRes)
 
 tclNum.forEach((el)=>{
     el.addEventListener('click',(evt)=>{
-        display.innerHTML+=evt.target.innerHTML
+        sinal=false
+        if(evt.target.innerHTML==","){
+            if(!decimal){
+                decimal=true
+                if(display.innerHTML=="0"){
+                    display.innerHTML="0,"
+                }else{
+                    display.innerHTML+=evt.target.innerHTML
+                }
+            }
+        }else{
+            if(display.innerHTML=="0"){
+                display.innerHTML=""
+            }
+            display.innerHTML+=evt.target.innerHTML
+        }
     })
 })
+
 tclOp.forEach((el)=>{
     el.addEventListener('click',(evt)=>{
-        display.innerHTML+=evt.target.innerHTML
+        if(!sinal){
+            sinal=true
+            if(display.innerHTML=="0"){
+                display.innerHTML=""
+            }
+            if(evt.target.innerHTML=="x"){
+                display.innerHTML+="*"
+            }else{
+                display.innerHTML+=evt.target.innerHTML
+            }
+        }
     })
 })
 
 tLimpar.addEventListener('click',()=>{
+    sinal=false
+    decimal=false
     display.innerHTML=0
+})
+
+tigual.addEventListener('click',(evt)=>{
+    sinal=false
+    decimal=false
+    const res=eval(display.innerHTML)
+    display.innerHTML=res
 })
